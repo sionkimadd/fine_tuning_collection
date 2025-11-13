df4e6f9fceeab8b38144db7666ad201497029fdf
sionkim <skim14@academic.rrc.ca>
2025-11-13 12:24:36 -0600

    Fix file name

--- logicalqubit/deberta-v3-large-news-classifier.py
+++ logicalqubit/deberta-v3-large-news-classifier.py
@@ -1,20 +1,18 @@
 from transformers import pipeline
 
-classifier = pipeline(
-    "text-classification",
-    model="logicalqubit/deberta-v3-large-business-news-sentiment-classifier",
-    tokenizer="logicalqubit/deberta-v3-large-business-news-sentiment-classifier",
-    top_k=None,
-    device=-1
-)
+pipe = pipeline("text-classification", model="logicalqubit/deberta-v3-large-news-classifier")
 
 texts = [
-    "AMD surges after event highlighting AI-driven growth keeps analysts bullish",
+    "Major nations agree on first-ever global fee on greenhouse gases with plan that targets shipping",
+    "US consumer sentiment plummets to second-lowest level on records going back to 1952",
+    "DARPA eyes companies targeting industrially useful quantum computers",
+    "Queen legend Brian May officially introduces Benson Boone as his new friend",
+    "Maple Leafs will be down a player against the Montreal Canadiens on Saturday",
+    "McGill discovery sheds new light on autism, intellectual disabilities",
+    "Coca-Cola Coliseum concertgoers exposed to measles: Toronto Public Health",
 ]
 
-results = classifier(texts)
-
-for text, scores in zip(texts, results):
-    print(f"\n>>> {text}")
-    for s in scores:
-        print(f"  {s['label']:>8} : {s['score']:.4f}")
+for text in texts:
+    result = pipe(text)[0]
+    print(f"Text: {text}")
+    print(f"Category: {result['label']} (Confidence: {result['score']:.4f})\n")
